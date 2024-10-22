63a0443b227428f577e3f9a8b8aff5616edb44d0
Dhanush-klm <167899716+Dhanush-klm@users.noreply.github.com>
2024-10-22 18:02:29 +0530

    Update index.cjs

--- backend/index.cjs
+++ backend/index.cjs
@@ -7,7 +7,7 @@ const app = express();
 const port = 5002;
 
 const corsOptions = {
-  origin: ['http://localhost:5173','http://localhost:5002'],
+  origin: ['http://localhost:5173','http://localhost:5002','https://bents-frontend-server.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -29,7 +29,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Flask backend URL
-const FLASK_BACKEND_URL = 'http://localhost:5000';
+const FLASK_BACKEND_URL = 'https://bents-llm-server.vercel.app';
 
 
 
