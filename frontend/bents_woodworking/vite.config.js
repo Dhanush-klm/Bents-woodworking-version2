f6bd22f8844d172fd004075e4e11e3b76bc17cb4
Dhanush-klm <167899716+Dhanush-klm@users.noreply.github.com>
2024-10-22 19:42:43 +0530

    Update vite.config.js

--- frontend/bents_woodworking/vite.config.js
+++ frontend/bents_woodworking/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src',
+      '@radix-ui/react-select': path.resolve(__dirname, 'node_modules/@radix-ui/react-select'),
     },
   },
 });
